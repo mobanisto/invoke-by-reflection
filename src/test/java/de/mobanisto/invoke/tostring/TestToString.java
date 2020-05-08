2b6e4a2067a68eaf2307568aa13326eaa1da7d48
Sebastian Kürten <sebastian@topobyte.de>
2020-05-08 09:44:48 +0200

    Create copy of test for toString()

---
+++ src/test/java/de/mobanisto/invoke/tostring/TestToString.java
@@ -0,0 +1,30 @@
+package de.mobanisto.invoke.tostring;
+
+import de.mobanisto.invoke.BaseTest;
+import de.mobanisto.invoke.MethodHandle;
+import de.mobanisto.invoke.MethodHandles;
+import de.mobanisto.invoke.MethodType;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TestToString extends BaseTest {
+
+    @Test
+    public void testNative() throws NoSuchMethodException, IllegalAccessException {
+        java.lang.invoke.MethodHandle search = java.lang.invoke.MethodHandles.publicLookup().findStatic(Arrays.class,
+                "binarySearch", java.lang.invoke.MethodType.methodType(
+                        int.class, double[].class, int.class, int.class, double.class));
+        Assert.assertEquals("MethodHandle(double[],int,int,double)int", search.toString());
+    }
+
+    @Test
+    public void testOur() throws NoSuchMethodException, IllegalAccessException {
+        MethodHandle search = MethodHandles.publicLookup().findStatic(Arrays.class,
+                "binarySearch", MethodType.methodType(
+                        int.class, double[].class, int.class, int.class, double.class));
+        Assert.assertEquals("MethodHandle(double[],int,int,double)int", search.toString());
+    }
+
+}
